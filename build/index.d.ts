f7fa47480420f00be432988f0ffdd6de3b20d3bd
Yu Li <YLiLarry@gmail.com>
2023-02-16 11:36:30 +0000

    Add build

---
+++ build/index.d.ts
@@ -0,0 +1,20 @@
+import { ReactNode } from "react";
+import TerminalInput from "./linetypes/TerminalInput";
+import TerminalOutput from "./linetypes/TerminalOutput";
+import "./style.css";
+export declare enum ColorMode {
+    Light = 0,
+    Dark = 1
+}
+export interface Props {
+    name?: string;
+    prompt?: string;
+    height?: string;
+    colorMode?: ColorMode;
+    children?: ReactNode;
+    onInput?: ((input: string) => void) | null | undefined;
+    startingInputValue?: string;
+}
+declare const Terminal: ({ name, prompt, height, colorMode, onInput, children, startingInputValue, }: Props) => JSX.Element;
+export { TerminalInput, TerminalOutput };
+export default Terminal;
